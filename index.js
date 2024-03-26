50b4d32dcb2ff52e4a72b13e06af0f8f06669e9f
diana <gududiana@gmail.com>
2024-03-26 09:09:18 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (someValue) {
+    const hQBlock = 42;
+    return Math.abs(someValue - hQBlock);
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    const blockToFeet = 264;
+    return distanceFromHqInBlocks(someValue) * blockToFeet;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    let distance = Math.abs(destination - start);
+    const blockToFeet = 264;
+    return distance * blockToFeet;
+  }
+
+  function calculatesFarePrice(start, destination) {
+
+    let distance = distanceTravelledInFeet(start, destination)
+
+    if (distance <= 400){
+      return 0;
+    } 
+      else if (distance <= 2000){
+      return (distanceTravelledInFeet(start, destination) -400) * 0.02;
+    } 
+      else if (distance <= 2500){
+      return 25;
+    }
+    else{
+      return 'cannot travel that far'
+    }
+  }
